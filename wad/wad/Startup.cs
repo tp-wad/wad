beb29b8eec80747b1ab137328c778ea06de24540
unknown <igsmar@gmail.com>
2015-10-30 13:08:06 -0700

    adding in base vs project

---
+++ wad/wad/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(wad.Startup))]
+namespace wad
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
